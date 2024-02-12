04c8adbb148fc19eec84eadcedaa3daf3c2905ae
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2024-02-12 17:06:12 +0700

    Add Test Cases for Get Tests

---
+++ Test Suites/Get Suites.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get Suites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>23c5fb46-1691-4462-921a-2d9d63623aa3</testSuiteGuid>
+</TestSuiteEntity>
