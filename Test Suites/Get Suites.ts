4ecac214ae787f84c89934795cb208608c3f7467
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2024-02-12 17:27:41 +0700

    Add Delete

--- Test Suites/Get Suites.ts
+++ Test Suites/Get Suites.ts
@@ -11,4 +11,24 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>23c5fb46-1691-4462-921a-2d9d63623aa3</testSuiteGuid>
+   <testCaseLink>
+      <guid>c7df1528-a86a-4030-9f3f-df74b5145efe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET/TC Get Photos Title</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>971920a0-8dd8-4315-9841-88444fbe3c7a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e916a895-3ca3-4993-8fa4-a1a46f6d229b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET/TC Get Username</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
